107d8bca6896384f74d082493b5e16e6e16e8364
dbs10000 <d.burnsidesmith@gmail.com>
2026-07-23 14:38:48 +0100

    Added the special_word function logic

---
+++ 02-loop-practice/src/special_word.py
@@ -0,0 +1,7 @@
+from src.special_word_functions.special_word_functions import vowel_counter, reverse_string
+
+def special_word(text):
+    print("-----------------------")
+    print(f"Your special arguments is: \n{text}")
+    vowel_counter(text)
+    reverse_string(text)
